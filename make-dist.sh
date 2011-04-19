d1591bd394e943a28dda82499479f432b2250566
a.p.clement <a.p.clement@gmail.com>
2011-04-19 21:04:10 +0000

    change group names

--- make-dist.sh
+++ make-dist.sh
@@ -1,4 +1,4 @@
-VERSION=0.4-SNAPSHOT
+VERSION=0.5-SNAPSHOT
 DIST=sdm-$VERSION
 
 rm -rf $DIST
@@ -10,5 +10,5 @@ cp -v core/conf/sdm-config.groovy $DIST/lib
 cp -v maven/provider/target/sdm-maven-provider-$VERSION.jar $DIST/lib
 cp -v maven/metadata/target/sdm-maven-metadata-$VERSION.jar $DIST/lib
 
-cp -v ~/.m2/repository/org/codehaus/groovy/groovy-all/1.7.2/groovy-all-1.7.2.jar $DIST/lib
-cp -v ~/.m2/repository/org/apache/ivy/ivy/2.1.0/ivy-2.1.0.jar $DIST/lib
+cp -v ${HOMEPATH}/.m2/repository/org/codehaus/groovy/groovy-all/1.7.2/groovy-all-1.7.2.jar $DIST/lib
+cp -v ${HOMEPATH}/.m2/repository/org/apache/ivy/ivy/2.1.0/ivy-2.1.0.jar $DIST/lib
